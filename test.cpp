6497e42282d1e02509d96964870139f1977984a5
Devin Sharpe <devin.sharpe@colorado.edu>
2021-02-09 12:06:45 -0700

    Added unit test for RemoveTwos

--- test.cpp
+++ test.cpp
@@ -3,3 +3,10 @@
 #include "functions_to_implement.cpp"
 #include <vector>
 
+TEST_CASE ("Divide by 2", "[removeTwos]") {
+	REQUIRE( RemoveTwos(16) == 1);
+	REQUIRE( RemoveTwos(7) == 7);
+	REQUIRE( RemoveTwos(26) == 13);
+	REQUIRE( RemoveTwos(52) == 13);
+}
+
