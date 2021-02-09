a79a108273c801aa2e260a07da83918002f3b510
akitomoya616 <493984502@qq.com>
2021-02-09 14:05:28 -0700

    fixed test cases

--- test.cpp
+++ test.cpp
@@ -3,13 +3,13 @@
 #include "functions_to_implement.cpp"
 #include <vector>
 
-TEST_CASE("checking positive values for Sum"){
+TEST_CASE("checking positive values","[sum]"){
 	std::vector<double> test{1,2};
 	REQUIRE(Sum(test)==3);
 	REQUIRE(Product(test)==2);
 }
 
-TEST_CASE("checking nagative values for Sum"){
+TEST_CASE("checking nagative values","[sum]"){
 	std::vector<double> test{-1,2};
 	REQUIRE(Sum(test)==1);
 	REQUIRE(Product(test)==-2);
